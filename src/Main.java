42726d2dc208aedca7f1ee5a9e8aa2875d6f583e
Mario <al03054433@tecmilenio.mx>
2025-01-31 18:52:30 -0600

    Add initial files for Java basics.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+                //if
+                int numero = 10;
+                if(numero > 5)
+                {
+                    System.out.println("El numero es mayor");
+                }
+                else{
+                    System.out.println("El numero es menor");
+                }
+                for(int i = 1; i <= 10; i++)
+                {
+                    System.out.println("Este es unn for: ");
+                    System.out.println(i);
+                }
+    }
+}
